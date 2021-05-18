801f4f5627102b36d6730d52a98bc5ee2ed5ffe9
opengeekslabdh <opengeekslabdh@gmail.com>
2021-05-18 14:51:24 +0300

    init

---
+++ index.ts
@@ -0,0 +1,9 @@
+import * as Loggers from './logger';
+
+function Logger (isDevelopment: true): typeof Loggers.CustomLogger;
+function Logger (isDevelopment: false): typeof Loggers.Logger;
+function Logger (isDevelopment: boolean): typeof Loggers.Logger | Loggers.CustomLogger {
+  return isDevelopment === true ? Loggers.CustomLogger : Loggers.Logger;
+}
+
+export default Logger;
