7c497f02dc9817e887930ad52db977d15b2c74d4
opengeekslabdh <opengeekslabdh@gmail.com>
2021-05-18 17:00:23 +0300

    fix: types

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import * as Loggers from './logger';
 
-function Logger (isDevelopment: true): typeof Loggers.CustomLogger;
-function Logger (isDevelopment: false): typeof Loggers.Logger;
-function Logger (isDevelopment: boolean): typeof Loggers.Logger | Loggers.CustomLogger {
+function Logger(isDevelopment: true): typeof Loggers.CustomLogger;
+function Logger(isDevelopment: false): typeof Loggers.Logger;
+function Logger(
+  isDevelopment: boolean,
+): typeof Loggers.Logger | typeof Loggers.CustomLogger;
+function Logger(
+  isDevelopment: boolean,
+): typeof Loggers.Logger | typeof Loggers.CustomLogger {
   return isDevelopment === true ? Loggers.CustomLogger : Loggers.Logger;
 }
 
